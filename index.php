7ca5269e17315e397534ebb2817df01682e6e3d3
herbie98 <osga.utj@gmail.com>
2021-07-30 19:10:06 -0500

    responsive login

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ session_destroy();//destruye la sesion cuando se regresa a esta pagina
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/index.css" type="text/css">
     <title>index</title>
 </head>
 <body>
